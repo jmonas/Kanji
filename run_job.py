1e69064ef3f3eeece2a17794a37f6ef051102272
Jack Monas <jmonas@princeton.edu>
2024-02-03 15:49:34 -0500

    -

--- run_job.py
+++ run_job.py
@@ -1,6 +1,4 @@
 import subprocess
-from huggingface_hub import hf_hub_download
-ckpt_path = hf_hub_download(repo_id="CompVis/stable-diffusion-v-1-4-original", filename="sd-v1-4-full-ema.ckpt", use_auth_token=True)
 
 BATCH_SIZE = 4
 N_GPUS = 2
