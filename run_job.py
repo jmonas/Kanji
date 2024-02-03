90769cf8729acebfbf163e47886316b9cc2b5f1c
Jack Monas <jmonas@princeton.edu>
2024-02-03 15:42:06 -0500

    -

--- run_job.py
+++ run_job.py
@@ -22,7 +22,7 @@ command = [
     "--check_val_every_n_epoch",
     "10",
     "--finetune_from",
-    ckpt_path,
+    "/scratch/network/jmonas/.cache/huggingface/hub/models--CompVis--stable-diffusion-v-1-4-original/snapshots/f0bb45b49990512c454cf2c5670b0952ef2f9c71/sd-v1-4-full-ema.ckpt",
     "data.params.batch_size=4",
     "lightning.trainer.accumulate_grad_batches=1",
     "data.params.validation.params.n_gpus=2"
